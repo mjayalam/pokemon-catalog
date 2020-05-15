c4050cfbcc1f9404da23e9730fb8bd7a98a1a5d6
Martin <martin.ayala0297@gmail.com>
2020-05-14 22:15:34 -0500

    Updates

---
+++ precache-manifest.3c16cc9cf830053dda066bd7f91a9b22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54732af52a63571e6c7bfe3bb7697335",
+    "url": "/pokemon-catalog/index.html"
+  },
+  {
+    "revision": "23e57f53db8de8217ca0",
+    "url": "/pokemon-catalog/static/css/main.9d811022.chunk.css"
+  },
+  {
+    "revision": "027d229fecf4e9ef6868",
+    "url": "/pokemon-catalog/static/js/2.28b81e8c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pokemon-catalog/static/js/2.28b81e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23e57f53db8de8217ca0",
+    "url": "/pokemon-catalog/static/js/main.93d405f4.chunk.js"
+  },
+  {
+    "revision": "720bc79bff9d0b20a2bc",
+    "url": "/pokemon-catalog/static/js/runtime-main.4fec0b36.js"
+  }
+]);
